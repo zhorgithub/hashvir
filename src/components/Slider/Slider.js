3606ee2e38490e35f03691cb9b7efa6bfa39b43f
Varazdat1994 <simonyan.94@mail.ru>
2020-08-22 00:33:59 +0400

    homePage Slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,31 +1,80 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 
 const Slider = () => {
-
-
     return (
-        <div className="herder-img">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-7">
-                        <h1 className="header-img-text">
-                            Բոլոր ֆինանսական հաշվիչները մեկ հարթակում
-		        </h1>
-                        <p className="header-img-p">Մեր հարթակում գտնվող բազմաթիվ հաշվիչների օգնությամբ կարող էք հաշվել Ձեր նախընտրած բանկացած հաշիվը առանց բանկ կամ հաշվապահական ծառայության դիմելու։</p>
-                        <div className="header-img-button">
-                            <button type="button" className="btn calc-btn">Հաշվել</button>
+      <Carousel>
+        <div>
+            <div className="herder-img">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-7">
+                            <h1 className="header-img-text">
+                                Բոլոր ֆինանսական հաշվիչները մեկ հարթակում
+                    </h1>
+                            <p className="header-img-p">Մեր հարթակում գտնվող բազմաթիվ հաշվիչների օգնությամբ կարող էք հաշվել Ձեր նախընտրած բանկացած հաշիվը առանց բանկ կամ հաշվապահական ծառայության դիմելու։</p>
+                            <div className="header-img-button">
+                                <button type="button" className="btn calc-btn">Հաշվել</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-5">
-                        <div className="img-back">
+                        <div className="col-md-5">
+                            <div className="img-back">
+                            </div>
+                            <img src="img/Artboard.png" className="calc-img" />
                         </div>
-                        <img src="img/Artboard.png" className="calc-img" />
                     </div>
                 </div>
             </div>
         </div>
-
+        <div>
+            <div className="herder-img">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-7">
+                            <h1 className="header-img-text">
+                                Բոլոր ֆինանսական հաշվիչները մեկ հարթակում
+                    </h1>
+                            <p className="header-img-p">Մեր հարթակում գտնվող բազմաթիվ հաշվիչների օգնությամբ կարող էք հաշվել Ձեր նախընտրած բանկացած հաշիվը առանց բանկ կամ հաշվապահական ծառայության դիմելու։</p>
+                            <div className="header-img-button">
+                                <button type="button" className="btn calc-btn">Հաշվել</button>
+                            </div>
+                        </div>
+                        <div className="col-md-5">
+                            <div className="img-back">
+                            </div>
+                            <img src="img/Artboard.png" className="calc-img" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div className="herder-img">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-7">
+                            <h1 className="header-img-text">
+                                Բոլոր ֆինանսական հաշվիչները մեկ հարթակում
+                    </h1>
+                            <p className="header-img-p">Մեր հարթակում գտնվող բազմաթիվ հաշվիչների օգնությամբ կարող էք հաշվել Ձեր նախընտրած բանկացած հաշիվը առանց բանկ կամ հաշվապահական ծառայության դիմելու։</p>
+                            <div className="header-img-button">
+                                <button type="button" className="btn calc-btn">Հաշվել</button>
+                            </div>
+                        </div>
+                        <div className="col-md-5">
+                            <div className="img-back">
+                            </div>
+                            <img src="img/Artboard.png" className="calc-img" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Carousel>
     )
 
 
