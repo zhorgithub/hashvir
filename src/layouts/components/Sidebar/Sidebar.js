402d374b1817e99c473427c43066821f98561d02
Varazdat <simonyan.94@mail.ru>
2020-09-09 16:35:04 +0400

    calc page sidebar

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -5,45 +5,67 @@ function Sidebar(props) {
     const { children } = props;
     return (
         <ul className="list-unstyled components">
+            <li>
+            <Link to='salary'>Աշխատավարձ</Link>
+            </li>
+            <li>
+                <Link to='realEstate'>Գույքահարկ</Link>
+            </li>
+            <li>
+                <Link to='cars'>Տրանսպորտային միջոցների գույքահարկ</Link>
+            </li>
+
+            <li>
+                <Link to='appa'>Ապպա</Link>
+            </li>
+            <li>
+                <a href="#">Տեխզննում</a>
+            </li>
             <li className="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Home</a>
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Վարկ</a>
                 <ul className="collapse list-unstyled" id="homeSubmenu">
                     <li>
-                        <a href="#">Home 1</a>
+                        <a href="#">Վարկ 1</a>
                     </li>
                     <li>
-                        <a href="#">Home 2</a>
+                        <a href="#">Վարկ 2</a>
                     </li>
                     <li>
-                        <a href="#">Home 3</a>
+                        <a href="#">Վարկ 3</a>
                     </li>
                 </ul>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
-                <ul className="collapse list-unstyled" id="pageSubmenu">
+            </li> 
+            <li className="active">
+                <a href="#avandSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Ավանդ</a>
+                <ul className="collapse list-unstyled" id="avandSubmenu">
                     <li>
-                        <Link to='cars'>Cars</Link>
+                        <a href="#">Ավանդ 1</a>
                     </li>
                     <li>
-                        <Link to='salary'>Աշխատավարձ</Link>
+                        <a href="#">Ավանդ 2</a>
                     </li>
                     <li>
-                        <Link to='appa'>Ապպա</Link>
-                    </li>
-                    <li>
-                        <Link to='realEstate'>Գույքահարկ</Link>
+                        <a href="#">Ավանդ 3</a>
                     </li>
                 </ul>
+            </li> 
+            <li>
+                <a href="#">Փոխարժեք</a>
             </li>
             <li>
-                <a href="#">Portfolio</a>
+                <a href="#">Մայրության նպաստ</a>
             </li>
             <li>
-                <a href="#">Contact</a>
+                <a href="#">Արձակուրդային</a>
+            </li>
+            <li>
+                <a href="#">Տրանսպորտային միջոցների մաքսազերծում </a>
+            </li>
+            <li>
+                <a href="#">Տրանսպորտային միջոցների հաշվառում</a>
+            </li>
+            <li>
+                <a href="#">Կոմունալներ</a>
             </li>
         </ul>
     )
