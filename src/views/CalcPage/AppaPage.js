1ac106bab316bb535f8245c1543a92d8dfa96f09
Zhora <zhorlev94@gmail.com>
2020-09-26 19:26:03 +0400

    no message

--- src/views/CalcPage/AppaPage.js
+++ src/views/CalcPage/AppaPage.js
@@ -8,7 +8,7 @@ const AppaPage = () => {
     const [transport, setTransport] = useState(1)
     const [busType, setBusType] = useState(false)
     const [goal, setGoal] = useState(1)
-    const [engine, setEngine] = useState(1)
+    const [engine, setEngine] = useState(0.8)
     const [price, setPrice] = useState()
     const HA = 33122
 
@@ -20,6 +20,7 @@ const AppaPage = () => {
     const handleTransportChange = (e) => {
 
         const vl = e.target.value
+
         if (vl == -1) {
             setBusType(true)
             setTransport(1.44)
@@ -28,10 +29,32 @@ const AppaPage = () => {
             setBusType(false)
             setTransport(vl)
         }
+
+
     }
 
     const handleSubmit = () => {
-        setPrice(Math.round(HA * transport * goal * engine * BM * ZHG / 1000) * 1000)
+        console.log(transport);
+        switch (transport) {
+
+            case '1':
+                setPrice(Math.round(HA * transport * goal * engine * BM * ZHG / 1000) * 1000)
+                break
+            case '1.185':
+                console.log('ssss');
+                setPrice(Math.round(HA * transport * engine * BM * ZHG / 1000) * 1000)
+                break
+            case 1.44:
+                setPrice(Math.round(HA * transport * BM * ZHG / 1000) * 1000)
+                break
+            case 1.33:
+                setPrice(Math.round(HA * transport * BM * ZHG / 1000) * 1000)
+                break
+            case 0.59:
+                setPrice(Math.round(HA * BM * ZHG / 1000) * 1000)
+                break
+        }
+
 
     }
 
@@ -45,7 +68,7 @@ const AppaPage = () => {
                             <div>
                                 <label className="appa-label">Տրանսպորտային միջոցի տեսակ</label>
                                 <br />
-                                <select className="appa-select" onChange={handleTransportChange}>
+                                <select className="appa-select" value={"1"} onChange={handleTransportChange}>
                                     <option value="1">Թեթև մարդատար </option>
                                     <option value="1.185">Բեռնատար (ներառյալ ուղևորաբեռնատար)</option>
                                     <option value="-1">Ավտոբուս (ներառյալ միկրոավտոբուս), տրոյլեբուս</option>
@@ -58,8 +81,8 @@ const AppaPage = () => {
                                 <div>
                                     <label className="appa-label-count">Նստատեղերի քանակ</label>
                                     <br />
-                                    <select className="appa-select-count" onChange={e => setTransport(e.target.value)}>
-                                        <option value="1.44" selected>Մինչև 17 (առանց վարորդի)</option>
+                                    <select className="appa-select-count" value={"1.44"} onChange={e => setTransport(e.target.value)}>
+                                        <option value="1.44" >Մինչև 17 (առանց վարորդի)</option>
                                         <option value="1.133">18 և ավել (առանց վարորդի)</option>
                                     </select>
                                 </div>
@@ -70,8 +93,8 @@ const AppaPage = () => {
                             <div>
                                 <label className="appa-label-type">Օգտագործման նպատակ</label>
                                 <br />
-                                <select className="appa-select-type" onChange={e => setGoal(e.target.value)}>
-                                    <option value="1" selected>Անձնական</option>
+                                <select className="appa-select-type" value={"1"} onChange={e => setGoal(e.target.value)}>
+                                    <option value="1" >Անձնական</option>
                                     <option value="1">Հանրային տրանսպորտ</option>
                                     <option value="1.8">Տաքսի/վարձակալություն</option>
                                     <option value="1.03">Ծառայողական/առևտրային</option>
@@ -84,27 +107,32 @@ const AppaPage = () => {
                             <br />'
                             {transport == 1 ?
                                 <div>
-                                    <select className="appa-select-power" onChange={e => setEngine(e.target.value)}>
-                                        <option value="0.8">Մինչև 80 ձիաուժ</option>
+                                    <select className="appa-select-power" value={"0.8"} onChange={e => setEngine(e.target.value)}>
+                                        <option value="0.8" >Մինչև 80 ձիաուժ</option>
                                         <option value="1">81-ից 140 ձիաուժ</option>
                                         <option value="1.38">141-ից 230 ձիաուժ</option>
                                         <option value="1.64">231 և ավելի ձիաուժ</option>
                                     </select>
                                 </div>
-                                : <div>
-                                    <select className="appa-select-power" onChange={e => setEngine(e.target.value)}>
+                                : <></>}
+                            {transport == 1.185 ?
+                                <div>
+                                    <select className="appa-select-power" value={"0.8"} onChange={e => setEngine(e.target.value)}>
                                         <option value="0.8">Մինչև 80 ձիաուժ</option>
                                         <option value="1">81-ից 140 ձիաուժ</option>
                                         <option value="1.09">141-ից 230 ձիաուժ</option>
                                         <option value="1.1">231 և ավելի ձիաուժ</option>
                                     </select>
-                                </div>}
+                                </div>
+                                :
+                                <></>
+                            }
                         </div>
                         <br />
                         <label className="appa-label-date">Ժամկետ</label>
                         <br />
-                        <select className="appa-select-date" onChange={e => setZHG(e.target.value)}>
-                            <option value="1" selected>1 տարի</option>
+                        <select className="appa-select-date" value={"1"} onChange={e => setZHG(e.target.value)}>
+                            <option value="1">1 տարի</option>
                             <option value="0.95">11 ամիս</option>
                             <option value="0.85">10 ամիս</option>
                             <option value="0.77">9 ամիս</option>
@@ -122,7 +150,7 @@ const AppaPage = () => {
                         <br />
                         <label className="appa-label-date">Բոնուս մալուսի դաս</label>
                         <br />
-                        <select className="appa-select-date" onChange={e => setBM(e.target.value)}>
+                        <select className="appa-select-date" value={"1"} onChange={e => setBM(e.target.value)}>
                             <option value="0․5">Դաս 1</option>
                             <option value="0․65">Դաս 2</option>
                             <option value="0․75">Դաս 3</option>
@@ -132,7 +160,7 @@ const AppaPage = () => {
                             <option value="0․91">Դաս 7</option>
                             <option value="0․94">Դաս 8</option>
                             <option value="0․97">Դաս 9</option>
-                            <option value="1" selected>Դաս 10</option>
+                            <option value="1" >Դաս 10</option>
                             <option value="1․04">Դաս 11</option>
                             <option value="1․08">Դաս 12</option>
                             <option value="1.12">Դաս 13</option>
