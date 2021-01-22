3db8ed5356a3891f00f05c4a9ea7d84313224489
Zhora <zhorlev94@gmail.com>
2021-01-23 02:15:35 +0400

    texznnum is added

--- src/views/CalcPage/AppaPage.js
+++ src/views/CalcPage/AppaPage.js
@@ -71,17 +71,17 @@ const AppaPage = () => {
                 <div className="top-text"><h3>ԱՊՊԱ Հաշվիչ</h3></div>
                 <div className="top-input">
                     <div className="appa">
-                            <div>
-                                <label className="appa-label">Տրանսպորտային միջոցի տեսակ</label><br />
-                                <select className="appa-select" onChange={handleTransportChange}>
-                                    <option value='1'>Թեթև մարդատար </option>
-                                    <option value="1.185">Բեռնատար (ներառյալ ուղևորաբեռնատար)</option>
-                                    <option value="-1">Ավտոբուս (ներառյալ միկրոավտոբուս), տրոյլեբուս</option>
-                                    <option value="0.59">Մոտոտրանսպորտ (ներառյալ մոտոցիկլ, տրիցիկլ, քվադրիցիկլ)</option>
-                                    <option value="0.59">Այլ</option>
-                                </select><br />
-                            </div>
-                            <div className="parent-select">
+                        <div>
+                            <label className="appa-label">Տրանսպորտային միջոցի տեսակ</label><br />
+                            <select className="appa-select" onChange={handleTransportChange}>
+                                <option value='1'>Թեթև մարդատար </option>
+                                <option value="1.185">Բեռնատար (ներառյալ ուղևորաբեռնատար)</option>
+                                <option value="-1">Ավտոբուս (ներառյալ միկրոավտոբուս), տրոյլեբուս</option>
+                                <option value="0.59">Մոտոտրանսպորտ (ներառյալ մոտոցիկլ, տրիցիկլ, քվադրիցիկլ)</option>
+                                <option value="0.59">Այլ</option>
+                            </select><br />
+                        </div>
+                        <div className="parent-select">
                             {busType ?
                                 <div>
                                     <label className="appa-label-count">Նստատեղերի քանակ</label><br />
@@ -91,84 +91,84 @@ const AppaPage = () => {
                                     </select><br />
                                 </div>
                                 : <></>}
-                        {transport == 1 ?
-                            <div>
-                                <label className="appa-label-type">Օգտագործման նպատակ</label><br />
-                                <select className="appa-select-type" onChange={e => setGoal(e.target.value)}>
-                                    <option value="1" >Անձնական</option>
-                                    <option value="1">Հանրային տրանսպորտ</option>
-                                    <option value="1.8">Տաքսի/վարձակալություն</option>
-                                    <option value="1.03">Ծառայողական/առևտրային</option>
-                                </select><br />
-                            </div>
-                            : <></>}
-                        {transport == 1 || transport == 1.185 ?
-                            
-                            <div>
-                                <label className="appa-label-power">ՏՄ շարժիչի հզորությունը</label><br />
+                            {transport == 1 ?
                                 <div>
-                                    <select className="appa-select-power" onChange={e => setEngine(e.target.value)}>
-                                        <option value="0.8" >Մինչև 80 ձիաուժ</option>
-                                        <option value="1">81-ից 140 ձիաուժ</option>
-                                        <option value="1.38">141-ից 230 ձիաուժ</option>
-                                        <option value="1.64">231 և ավելի ձիաուժ</option>
+                                    <label className="appa-label-type">Օգտագործման նպատակ</label><br />
+                                    <select className="appa-select-type" onChange={e => setGoal(e.target.value)}>
+                                        <option value="1" >Անձնական</option>
+                                        <option value="1">Հանրային տրանսպորտ</option>
+                                        <option value="1.8">Տաքսի/վարձակալություն</option>
+                                        <option value="1.03">Ծառայողական/առևտրային</option>
                                     </select><br />
                                 </div>
-                            
-                            </div>
+                                : <></>}
+                            {transport == 1 || transport == 1.185 ?
 
-                            : <></>}
-                            </div>  <div className="parent-select">
+                                <div>
+                                    <label className="appa-label-power">ՏՄ շարժիչի հզորությունը</label><br />
+                                    <div>
+                                        <select className="appa-select-power" onChange={e => setEngine(e.target.value)}>
+                                            <option value="0.8" >Մինչև 80 ձիաուժ</option>
+                                            <option value="1">81-ից 140 ձիաուժ</option>
+                                            <option value="1.38">141-ից 230 ձիաուժ</option>
+                                            <option value="1.64">231 և ավելի ձիաուժ</option>
+                                        </select><br />
+                                    </div>
+
+                                </div>
+
+                                : <></>}
+                        </div>  <div className="parent-select">
                             <div>
-                        <label className="appa-label-date">Ժամկետ</label><br />
-                        <select className="appa-select-date" onChange={e => setZHG(e.target.value)}>
-                            <option value="1">1 տարի</option>
-                            <option value="0.95">11 ամիս</option>
-                            <option value="0.85">10 ամիս</option>
-                            <option value="0.77">9 ամիս</option>
-                            <option value="0.7">8 ամիս</option>
-                            <option value="0.65">7 ամիս</option>
-                            <option value="0.6">6 ամիս</option>
-                            <option value="0.5">5 ամիս</option>
-                            <option value="0.4">4 ամիս</option>
-                            <option value="0.33">3 ամիս</option>
-                            <option value="0.25">2 ամիս*</option>
-                            <option value="0.2">16 օրից մինչև 1 ամիս ներառյալ</option>
-                            <option value="0.15">11 օրից մինչև 15 օր ներառյալ</option>
-                            <option value="0.1">10 օր</option>
-                        </select><br />
-                       </div>
-                       <div>
-                        <label className="appa-label-date">Բոնուս մալուսի դաս</label><br />
-                        <select className="appa-select-date" value={BM} onChange={e => setBM(e.target.value)}>
-                            <option value="0․5">Դաս 1</option>
-                            <option value="0․65">Դաս 2</option>
-                            <option value="0․75">Դաս 3</option>
-                            <option value="0․82">Դաս 4</option>
-                            <option value="0․85">Դաս 5</option>
-                            <option value="0․88">Դաս 6</option>
-                            <option value="0․91">Դաս 7</option>
-                            <option value="0․94">Դաս 8</option>
-                            <option value="0․97">Դաս 9</option>
-                            <option value="1" >Դաս 10</option>
-                            <option value="1․04">Դաս 11</option>
-                            <option value="1․08">Դաս 12</option>
-                            <option value="1.12">Դաս 13</option>
-                            <option value="1.16">Դաս 14</option>
-                            <option value="1.24">Դաս 15</option>
-                            <option value="1.32">Դաս 16</option>
-                            <option value="1.4">Դաս 17</option>
-                            <option value="1.44">Դաս 18</option>
-                            <option value="2">Դաս 19</option>
-                            <option value="2.5">Դաս 20</option>
-                            <option value="2․5">Դաս 21</option>
-                            <option value="2․5">Դաս 22</option>
-                        </select><br />
+                                <label className="appa-label-date">Ժամկետ</label><br />
+                                <select className="appa-select-date" onChange={e => setZHG(e.target.value)}>
+                                    <option value="1">1 տարի</option>
+                                    <option value="0.95">11 ամիս</option>
+                                    <option value="0.85">10 ամիս</option>
+                                    <option value="0.77">9 ամիս</option>
+                                    <option value="0.7">8 ամիս</option>
+                                    <option value="0.65">7 ամիս</option>
+                                    <option value="0.6">6 ամիս</option>
+                                    <option value="0.5">5 ամիս</option>
+                                    <option value="0.4">4 ամիս</option>
+                                    <option value="0.33">3 ամիս</option>
+                                    <option value="0.25">2 ամիս*</option>
+                                    <option value="0.2">16 օրից մինչև 1 ամիս ներառյալ</option>
+                                    <option value="0.15">11 օրից մինչև 15 օր ներառյալ</option>
+                                    <option value="0.1">10 օր</option>
+                                </select><br />
+                            </div>
+                            <div>
+                                <label className="appa-label-date">Բոնուս մալուսի դաս</label><br />
+                                <select className="appa-select-date" value={BM} onChange={e => setBM(e.target.value)}>
+                                    <option value="0.5">Դաս 1</option>
+                                    <option value="0.65">Դաս 2</option>
+                                    <option value="0.75">Դաս 3</option>
+                                    <option value="0.82">Դաս 4</option>
+                                    <option value="0.85">Դաս 5</option>
+                                    <option value="0.88">Դաս 6</option>
+                                    <option value="0.91">Դաս 7</option>
+                                    <option value="0.94">Դաս 8</option>
+                                    <option value="0.97">Դաս 9</option>
+                                    <option value="1" >Դաս 10</option>
+                                    <option value="1.04">Դաս 11</option>
+                                    <option value="1.08">Դաս 12</option>
+                                    <option value="1.12">Դաս 13</option>
+                                    <option value="1.16">Դաս 14</option>
+                                    <option value="1.24">Դաս 15</option>
+                                    <option value="1.32">Դաս 16</option>
+                                    <option value="1.4">Դաս 17</option>
+                                    <option value="1.44">Դաս 18</option>
+                                    <option value="2">Դաս 19</option>
+                                    <option value="2.5">Դաս 20</option>
+                                    <option value="2.5">Դաս 21</option>
+                                    <option value="2.5">Դաս 22</option>
+                                </select><br />
+                            </div>
                         </div>
-                    </div>
                         <br />
                         <br />
-                       <button type="button" onClick={handleSubmit} className="btn calc-btn">Հաշվել</button>
+                        <button type="button" onClick={handleSubmit} className="btn calc-btn">Հաշվել</button>
                     </div>
                 </div>
                 <h3> {price ? "Արդյունք - " + price : ""}</h3>
--- src/views/CalcPage/TeInspectionPage.js
+++ src/views/CalcPage/TeInspectionPage.js
@@ -1,46 +1,170 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Switch } from 'react-router-dom'
 
 
-const  TeInspectionPage = () => {
+const TeInspectionPage = () => {
+    const [taretiv, setTaretiv] = useState()
+    const [dzi, setDzi] = useState()
+    const [dziIndex, setDziIndex] = useState()
+    const [znnumFee, setZnnumFee] = useState(5500)
+    const [bnapTax, setBnapTax] = useState()
+    const [ready, setReady] = useState(false)
+    const ktron = 2500
+
+    const todayYear = new Date().getFullYear()
+    const arr1 = [
+        [2.5, 2.5, 2.5, 3, 3.5, 4, 4.5, 5],
+        [5, 5, 5, 6, 7, 8, 9, 10],
+        [7.5, 7.5, 7.5, 9, 10.5, 12, 13.5, 15],
+        [10, 10, 10, 12, 14, 16, 18, 20],
+        [12.5, 12.5, 12.5, 15, 17.5, 20, 22.5, 25],
+        [15, 15, 15, 18, 21, 24, 27, 30],
+        [17.5, 17.5, 17.5, 21, 24.5, 28, 31.5, 35],
+        [25, 25, 25, 30, 35, 40, 45, 50]
+    ]
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // console.log(znnumFee);
+        // console.log(bnapTax);
+        console.log(taretiv);
+        console.log(dzi);
+        console.log(arr1[dziIndex][taretiv]);
+        setBnapTax(dzi * arr1[dziIndex][taretiv]);
+
+        setReady(true)
+    }
+
+    const handleChangeTransport = (e) => {
+        const a = e.target.value
+        console.log(a);
+        switch (a) {
+            case '1':
+                setZnnumFee(5500)
+                break;
+            case '2':
+                console.log('ssss');
+                setZnnumFee(7500)
+                break;
+            case '3':
+                setZnnumFee(10500)
+                break;
+            case '4':
+                setZnnumFee(7500)
+                setBnapTax(5000)
+                break;
+            case '5':
+                setZnnumFee(10500)
+                setBnapTax(10000)
+                break;
+            case '6':
+                setZnnumFee(10500)
+                setBnapTax(15000)
+                break;
+            case '7' || 8:
+                setZnnumFee(3500)
+                setBnapTax(0)
+                break;
+        }
+
+    }
+
+    const handleChangeDzi = (e) => {
+        const a = (e.target.value)
+        setDzi(a)
+        switch (true) {
+            case a > 0 && a <= 50:
+                setDziIndex(0)
+                break;
+            case a >= 51 && a <= 80:
+                setDziIndex(1)
+                break;
+            case a >= 81 && a <= 100:
+                setDziIndex(2)
+                break;
+            case a >= 101 && a <= 150:
+                setDziIndex(3)
+                break;
+            case a >= 151 && a <= 200:
+                setDziIndex(4)
+                break;
+            case a >= 201 && a <= 250:
+                setDziIndex(5)
+                break;
+            case a >= 251 && a <= 300:
+                setDziIndex(6)
+                break;
+            case a >= 301:
+                setDziIndex(7)
+                break;
+
+        }
+    }
+
+    const handleChangeTaretiv = (e) => {
+        if (e.target.value >= 8) {
+            setTaretiv(7)
+        } else {
+            setTaretiv(e.target.value)
+        }
+    }
 
     return (
         <>
-            <div className="top">
-                <div className="top-text"><h3>Տեխզննում</h3></div>
-                <div className="top-input">
-                    <div className="appa">
-                        <div className="parent-select">
-                            <div>
-                               
-                            </div>
-                            <div>
-                             
-                            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="salary">
+                    <div className="top-text"><h3>Տեխնիկական զննություն</h3></div>
+                    <hr className="sep" />
+                    <div className="parent-select">
+                        <div>
+                            <label className="appa-label">Տրանսպորտային միջոցի տեսակ</label><br />
+                            <select className="appa-select" onChange={handleChangeTransport} >
+                                <option value='1'>Թեթև մարդատար </option>
+                                <option value='2'>Ավտոբուս (մինչև 17 նստատեղով) </option>
+                                <option value='3'>Ավտոբուս (17-ից ավել նստատեղով) </option>
+                                <option value="4">Բեռնատար (մինչև 3․5 տոննա զանգվածով)</option>
+                                <option value="5">Բեռնատար (3․5 - 12 տոննա  զանգվածով)</option>
+                                <option value="6">Բեռնատար (12 տոննա զանգվածը գերազանցող)</option>
+                                <option value="7">Կցորդներ կամ կիսակցորդներ</option>
+                                <option value="8">Մոտոցիկլետներ կամ մոպեդներ</option>
+                            </select><br />
                         </div>
-                        <br />
-                        <div className="parent-select">
-                            <div>
-                              
-                            </div>
-                            <div>
-                               
-                            </div>
+                        <div>
+                            <label className="salary-label">Տարեթիվ</label><br />
+                            <select className="salary-select" onChange={handleChangeTaretiv} >
+                                {Array.from(Array(todayYear - 1949).keys()).map(i => (
+                                    <option key={i} value={i}>{todayYear - i}</option>
+                                ))}
+
+                            </select>
                         </div>
-                        <br />
-                        <center><button type="button" className="btn calc-btn">Հաշվել</button></center>
+
                     </div>
+                    <div>
+                        <label className="salary-tt"  > Ձիաուժը</label><br />
+                        <input type='number' className="salary-price" onChange={handleChangeDzi} placeholder="Օրինակ ՝ 120" />
+                    </div>
+
+
+
+
+                    <button type="submit" className="btn calc-btn"  >Հաշվել</button>
+                    {ready ?
+                        <>
+                            <h4>Տեխնիկական զննության ծառայության վճար - {znnumFee}</h4>
+                            <h4>Բնապահպանության հարկ - {bnapTax}</h4>
+                            <h4>Տեխնիկական զննության կտրոն - {ktron}</h4>
+                            <h3>Ընդամենը - {znnumFee + bnapTax + ktron} </h3>
+                            {/* <h3> {startSalary ? 'Աշխատավարձ - ' + startSalary : ''}</h3>
+                            <h2> {ekamtayin ? 'Եկամտային հարկ - ' + ekamtayin : ''}</h2>
+                            <h2> {kutakayin ? 'Կուտակային կենս․ - ' + kutakayin : ''}</h2>
+                            <h2>Դրոշմանշային վճար 1000</h2>
+                            <h2> {finalSalary > 1000 ? 'Արդյունք - ' + finalSalary : ''}</h2> */}
+                        </>
+                        : null}
                 </div>
-            </div>
-            <div className="bottom">
-                <div className="bottom-div">
-                    <div className="top-text"><h3>Result</h3></div>
-                    <center><div className="bottom-input">
-                        <input type="text" name="" /><br />
-                        <input type="text" name="" /><br />
-                        <input type="text" name="" /><br />
-                    </div></center>
-                </div>
-            </div>
+
+            </form>
         </>
     )
 }
@@ -48,4 +172,4 @@ const  TeInspectionPage = () => {
 
 
 
-export default  TeInspectionPage
+export default TeInspectionPage
